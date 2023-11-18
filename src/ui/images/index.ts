b4c0997681a31aa63a520b8eaa6b06d842538c48
diogoesteves <diogo.esteves@eino.ai>
2023-11-18 22:14:33 +0000

    Init repo

--- src/ui/images/index.ts
+++
@@ -1,5 +0,0 @@
-import CircleSVG from './circle-bg.svg';
-import ClockSVG from './clock.svg';
-import PlanSVG from './plan.svg';
-
-export { CircleSVG, ClockSVG, PlanSVG };
--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,4 +1,3 @@
 export * from './components';
 export * from './icons';
-export * from './images';
 export * from './styles';
